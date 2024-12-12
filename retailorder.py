0a0ddbe43f12a2891ba203adfd4ea5435a629e65
Durgadevi3105 <durga.n3105@gmail.com>
2024-12-12 13:18:51 +0530

    Update retailorder.py

--- retailorder.py
+++ retailorder.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyPLBBv49qKs3iVd0DIGFf0C",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
